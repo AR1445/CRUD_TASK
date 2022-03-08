e97018c995c248a3390b3a77ebe8ac70493f6516
AR1445 <101051929+AR1445@users.noreply.github.com>
2022-03-08 15:05:29 +0530

    Update sortNumbers.java

--- crud/src/main/java/com/example/sortNumbers.java
+++ crud/src/main/java/com/example/sortNumbers.java
@@ -5,10 +5,24 @@ import java.util.Arrays;
 public class sortNumbers {
 
 	public static void main(String[] args) {
-		try {
-			int[] uoNumber = {-1,2,-3,4,-5};
-			Arrays.parallelSort(uoNumber);
-			 Arrays.stream(uoNumber).forEach(n -> System.out.print(n + " "));
+		int[] uoInt = {1,-2,3,-4,-1,5,6};
+		int count = 1;
+		int size = uoInt.length;
+		int  temp = 0;
+		try {	
+       		   for (int i = 0; i < size; i++) {
+            		if (uoInt[i] < 0) {
+               		 if (i != count) {
+                  	  temp = uoInt[i];
+                  	  uoInt[i] = uoInt[count];
+                  	  uoInt[count] = temp;
+               		 }
+              		  count++;
+            	}
+       	 }
+		
+		System.out.println(uoInt);
+	
 			
 		} catch (Exception e) {
 			e.printStackTrace();
